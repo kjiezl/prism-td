b5546233f2cdb258b53f19700e9b0b6be8f23fcf
zxk <kearlsaint@gmail.com>
2024-04-23 23:09:23 +0800

    e

--- js/main-menu.js
+++ js/main-menu.js
@@ -16,39 +16,38 @@ $(function() {
             alert("Please rotate to landscape mode");
         }
     }, 100, window);
-});
-
-$(document).ready(function () {
+    
     $('.menu-toggle-btn').click(function () {
       $('.menu-toggle-btn').toggleClass("effect");
       $('.menu-list').toggleClass("effect");
     });
-});
 
-$("#info-button").click(() =>{
-    $(".info-container, .info-bg").toggleClass("active");    
-    return false;
-})
+    $("#info-button").click(() =>{
+        $(".info-container, .info-bg").toggleClass("active");    
+        return false;
+    })
 
-$(".info-close").click(() => {
-    $(".info-container, .info-bg").toggleClass("active");    
-})
+    $(".info-close").click(() => {
+        $(".info-container, .info-bg").toggleClass("active");    
+    })
 
-$("#settings-button").click(() => {alert("settings menu coming soon")});
-$("#feedback-button").click(() => {alert("feedback button coming soon")});
+    $("#settings-button").click(() => {alert("settings menu coming soon")});
+    $("#feedback-button").click(() => {alert("feedback button coming soon")});
 
-$("button, svg, .menu-item, .menu-toggle-btn").click(() => {buttonSound.play()})
+    $("button, svg, .menu-item, .menu-toggle-btn").click(() => {buttonSound.play()})
 
-let playingMusic = false;
-  $(".musicToggle").click(() => {
-    if(!playingMusic){
-        mainMusic.play();
-        mainMusic.volume = 0.3;
-        playingMusic = true;
-    } else{
-        mainMusic.pause();
-        playingMusic = false;
-    }
-    $("#musicPause").toggle();
-    $("#musicButton").toggle();
+    let playingMusic = false;
+      $(".musicToggle").click(() => {
+        if(!playingMusic){
+            mainMusic.play();
+            mainMusic.volume = 0.3;
+            playingMusic = true;
+        } else{
+            mainMusic.pause();
+            playingMusic = false;
+        }
+        $("#musicPause").toggle();
+        $("#musicButton").toggle();
+    });
+
 });
